ddb0df62a64aba7c1d7378fe5bae75cf108b50ba
Jorge Pedret <jorge@chloi.io>
2014-07-11 12:54:39 -0700

    Initial commit (starting setup of env)

---
+++ index.js
@@ -0,0 +1,1 @@
+var rolodex = require("rolodex");
---
+++ test/basic.js
@@ -0,0 +1,8 @@
+var app     = require("../index");
+var should  = require('should');
+
+describe("basic", function(){
+  it("should work", function (done) {
+    done();
+  })
+});
